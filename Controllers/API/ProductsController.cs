6623f4be39571fed9abfe54f39a09d76403f81d8
andre03fernandes <andre2411fernandes@gmail.com>
2022-07-07 03:29:06 +0100

    API products controller

---
+++ Controllers/API/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperShop.Data;
+
+namespace SuperShop.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_productRepository.GetAll()); // Busca os produtos todos através do repositório - Ok - embrulha tudo num json, testar no postman
+        }
+    }
+}
