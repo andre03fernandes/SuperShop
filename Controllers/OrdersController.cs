e746754d7a04f0267cb11d08ca373e95370f30f8
andre03fernandes <andre2411fernandes@gmail.com>
2022-08-05 17:20:32 +0100

    Add items on detail temp

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SuperShop.Data;
 using SuperShop.Models;
+using System.Threading.Tasks;
 
 
 namespace SuperShop.Controllers
@@ -39,6 +38,18 @@ namespace SuperShop.Controllers
                 Quantity = 1,
                 Products = _productRepository.GetComboProducts()
             };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(AddItemViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _orderRepository.AddItemToOrderAsync(model, this.User.Identity.Name);
+                return RedirectToAction("Create");
+            }
 
             return View(model);
         }
--- Data/OrderRepository.cs
+++ Data/OrderRepository.cs
@@ -5,6 +5,7 @@
     using Microsoft.EntityFrameworkCore;
     using SuperShop.Data.Entities;
     using SuperShop.Helpers;
+    using SuperShop.Models;
 
     public class OrderRepository : GenericRepository<Order>, IOrderRepository
     {
@@ -53,6 +54,61 @@
                 .Include(p => p.Product)
                 .Where(o => o.User == user)
                 .OrderBy(o => o.Product.Name);
+        }
+
+        public async Task AddItemToOrderAsync(AddItemViewModel model, string userName)
+        {
+            var user = await _userHelper.GetUserByEmailAsync(userName);
+            if(user == null)
+            {
+                return;
+            }
+
+            var product = await _context.Products.FindAsync(model.ProductId);
+            if(product == null)
+            {
+                return;
+            }
+
+            var orderDetailTemp = await _context.OrderDetailsTemp
+                .Where(odt => odt.User == user && odt.Product == product)
+                .FirstOrDefaultAsync();
+
+            if(orderDetailTemp == null)
+            {
+                orderDetailTemp = new OrderDetailTemp
+                {
+                    Price = product.Price,
+                    Product = product,
+                    Quantity = model.Quantity,
+                    User = user,
+                };
+
+                _context.OrderDetailsTemp.Add(orderDetailTemp);
+            }
+            else
+            {
+                orderDetailTemp.Quantity += model.Quantity;
+                _context.OrderDetailsTemp.Update(orderDetailTemp);
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task ModifyOrderDetailTempQuantityAsync(int id, double quantity)
+        {
+            var orderDetailsTemp = await _context.OrderDetailsTemp.FindAsync(id);
+            if(orderDetailsTemp == null)
+            {
+                return;
+            }
+
+            orderDetailsTemp.Quantity += quantity;
+            if(orderDetailsTemp.Quantity > 0)
+            {
+                _context.OrderDetailsTemp.Update(orderDetailsTemp);
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }
