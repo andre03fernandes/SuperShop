00fdb22dc548c414809f1b53d99e8be6dc780f0d
andre03fernandes <andre2411fernandes@gmail.com>
2022-08-08 15:55:09 +0100

    Cascade Delete Rule

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -157,11 +157,27 @@ namespace SuperShop.Controllers
         // POST: Products/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(int id) // CRIAR UMA VIEW COM UMA MENSAGEM MAIS BONITA
         {
             var product = await _productRepository.GetByIdAsync(id);
-            await _productRepository.DeleteAsync(product);
-            return RedirectToAction(nameof(Index));
+
+            try
+            {
+                //throw new Exception("Exceção de Teste");
+                await _productRepository.DeleteAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException ex)
+            {
+                if (ex.InnerException != null && ex.InnerException.Message.Contains("DELETE"))
+                {
+                    ViewBag.ErrorTitle = $"{product.Name} provavelmente está a ser usado!";
+                    ViewBag.ErrorMessage = $"{product.Name} não pode ser apagado visto haverem encomendas que o usam.<br><br>" +
+                        $"Experimente primeiro apagar todas as encomendas que estão a usar, " +
+                        $"e torne novamente a apagá-lo"; 
+                }
+                return View("Error");
+            }
         }
 
         public IActionResult ProductsNotFound()
--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SuperShop.Data.Entities;
+using System.Linq;
 
 namespace SuperShop.Data
 {
@@ -15,5 +16,21 @@ namespace SuperShop.Data
         public DbSet<OrderDetailTemp> OrderDetailsTemp { get; set; }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        // Habilitar a regra de apagar em cascata (Cascade Delete Rule)
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    var cascadeFKs = modelBuilder.Model
+        //        .GetEntityTypes()
+        //        .SelectMany(t => t.GetForeignKeys())
+        //        .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade);
+
+        //    foreach(var fk in cascadeFKs)
+        //    {
+        //        fk.DeleteBehavior = DeleteBehavior.Restrict;
+        //    }
+
+        //    base.OnModelCreating(modelBuilder);
+        //}
     }
 }
